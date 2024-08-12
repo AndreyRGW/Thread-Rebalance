e0107ad87348c5a9e7a9020c3d73463e9bc5ae6c
AndreyRGW <RGWyo@ANDREY-PC>
2024-08-13 01:50:19 +0300

    GUI 1.2

--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,39 @@ namespace ThreadRebalanceGUI
 {
     public partial class Form1 : Form
     {
+        private System.Windows.Forms.Timer processCheckTimer;
+        private int selectedProcessId = -1;
+        private Process selectedProcessInstance;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayProcess();
+            InitializeProcessCheckTimer();
+            button1.Enabled = false; // Изначально кнопка "Rebalance" неактивна
+        }
+
+        private void InitializeProcessCheckTimer()
+        {
+            processCheckTimer = new System.Windows.Forms.Timer();
+            processCheckTimer.Interval = 1000; // Проверка каждую секунду
+            processCheckTimer.Tick += ProcessCheckTimer_Tick;
+            processCheckTimer.Start();
+        }
+
+        private void ProcessCheckTimer_Tick(object sender, EventArgs e)
+        {
+            if (selectedProcessInstance != null && selectedProcessInstance.HasExited)
+            {
+                label3.Text = "none";
+                button1.Enabled = false;
+                button2.Enabled = false;
+                label5.Text = "not running";
+                selectedProcessInstance = null;
+                selectedProcessId = -1;
+            }
+        }
+
         private void DisplayProcess()
         {
             var processes = Process.GetProcesses()
@@ -23,14 +56,6 @@ namespace ThreadRebalanceGUI
                                    .Select(p => $"{p.ProcessName} (ID: {p.Id})")
                                    .ToList();
             listBox1.Items.AddRange(processes.ToArray());
-        }
-
-        private int selectedProcessId = -1;
-
-        public Form1()
-        {
-            InitializeComponent();
-            DisplayProcess();
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -49,6 +74,9 @@ namespace ThreadRebalanceGUI
 
                 string selectedProcessName = selectedProcess.Substring(0, startIndex - 6).Trim();
                 label3.Text = $"{selectedProcessName} (ID: {selectedProcessId})";
+
+                selectedProcessInstance = Process.GetProcessById(selectedProcessId);
+                button1.Enabled = true; // Активируем кнопку "Rebalance" после выбора процесса
             }
             else
             {
@@ -56,7 +84,6 @@ namespace ThreadRebalanceGUI
             }
         }
 
-        // rebalance
         private async void button1_Click(object sender, EventArgs e)
         {
             if (selectedProcessId < 0)
@@ -89,7 +116,6 @@ namespace ThreadRebalanceGUI
         {
         }
 
-        // stop
         private void button2_Click(object sender, EventArgs e)
         {
             button1.Enabled = true;
